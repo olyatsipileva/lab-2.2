4c9ed8dda4da6aeaaba76b06b3a05b23385c59d3
igor57690 <igor57690@yandex.ru>
2021-12-20 22:55:16 +0800

    Добавьте файлы проекта.

---
+++ lab 2.2/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace lab_2._2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            try
+            {
+                using (StreamReader sr = new StreamReader("save.txt"))
+                {
+                    textBox1.Text = sr.ReadLine();
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label2.Text = Logic.Subsequence(textBox1.Text);
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {            
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '-') && (e.KeyChar != ' '))
+            {
+                e.Handled = true;
+            }
+
+            /*if ((e.KeyChar == '-') || (e.KeyChar == ' '))
+            {
+                e.Handled = true;
+            }*/
+            
+        }
+
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                using (StreamWriter sw = new StreamWriter("save.txt", false, System.Text.Encoding.Default))
+                {
+                    sw.WriteLine(textBox1.Text);
+
+                }
+            }
+            catch (Exception d)
+            {
+                Console.WriteLine(d.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            label2.Text = "";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ lab 2.2/Logic.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab_2._2
+{
+    public static class Logic
+    {
+        public static String Subsequence(string str)
+        {
+            string[] nums = str.Split(' ');
+            int k = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != "")
+                {
+                    k++;
+                }
+            }
+            int[] array = new int[k];
+            k = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != "")
+                {
+                    array[k] = Convert.ToInt32(nums[i]);
+                    k++;
+                }
+            }
+
+            bool mean = true;
+            int count = 0;
+            for (int i = 0; i < array.Length; i++)
+            { 
+                for (int j = i; j < array.Length; j++)
+                {
+                    if (array[i] == array[j] && i != j)
+                    {
+                        count++;
+                        if (count == array.Length)
+                        {
+                            return "Ошибка! Числа не должны быть одинаковыми!";
+                        }
+                    }
+                    if (array[i] > array[j])
+                    {
+                        mean = false;
+                    }
+                }
+            }
+            if (mean)
+            {
+                return "Последовательность чисел является упорядоченной по возрастанию!";
+            }
+            else
+            {
+                return "Последовательность чисел не является упорядоченной по возрастанию!";
+            }
+        }
+
+    }
+}
